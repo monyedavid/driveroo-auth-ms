77c70244500722dc20d96e43821122d70e340c50
rasheeddavid <griffinc317@gmail.com>
2019-07-28 12:21:57 +0100

    staging 1 | mongoose | user model

---
+++ src/models/User.ts
@@ -0,0 +1,56 @@
+import * as mongoose from "mongoose";
+import * as bcrypt from "bcryptjs";
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        confirmed: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        forgotPasswordLock: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        avatar: {
+            type: String
+        }
+    },
+    { timestamps: true }
+);
+
+/**
+ *   @BeforeInsert()
+ */
+
+const hashedPassword = async (password: string) => {
+    return await bcrypt.hash(password, 10);
+};
+
+UserSchema.pre("save", async function() {
+    if (this.isNew) {
+        const newpassword = await hashedPassword((this as any).password);
+        (this as any).password = newpassword;
+    }
+});
+
+export const User = mongoose.model("users", UserSchema);
