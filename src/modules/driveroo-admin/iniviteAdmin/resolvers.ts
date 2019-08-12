24e0660eebe6d4ee8956b777b3958b3ee6b08185
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-12 09:33:30 +0100

    reg | new user

--- src/modules/driveroo-admin/iniviteAdmin/resolvers.ts
+++ src/modules/driveroo-admin/iniviteAdmin/resolvers.ts
@@ -1,10 +1,13 @@
 import { ResolverMap } from "../../../types/graphql-utile";
+import { UAdmin } from "../../../class/auth/admin.invitation";
 
 export const resolvers: ResolverMap = {
     Query: {
         bye: () => "THSES NUTS SON!"
     },
     Mutation: {
-        admin_: async (_, __, { url }) => {}
+        admin_: async (_, { email, mobile }: GQL.IAdminLinkParams, { url }) => {
+            return await new UAdmin(url).registerNewUser({ email, mobile });
+        }
     }
 };
