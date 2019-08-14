383827292fd1362b34713160da9cafae8d5fe0af
Joshua Oluikpe <joshuaoluikpe@gmail.com>
2019-08-14 10:53:21 +0100

    redis |url

--- deploy_server_heroku.sh
+++ deploy_server_heroku.sh
@@ -2,7 +2,5 @@
 yarn build:server
 docker build -t lilmakijr/d-auth-ms:latest .
 docker push lilmakijr/d-auth-ms
-git add . 
-git commit -m "new deploy | Docker Script | latest"
 heroku container:push web
 heroku container:release web
